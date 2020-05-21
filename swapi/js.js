e09518dbdfa106838671fc7bef58352a478c30ff
renaud dor <renaud.dor@gmail.com>
2020-05-21 19:46:33 +0200

    code methods and promises for 'fastest ship' exercise

--- swapi/js.js
+++ swapi/js.js
@@ -1,29 +1,14 @@
-url = 'https://swapi.dev/api/starships/5/';
+const url = 'https://swapi.dev/api/starships/2/';
+const numberOfPassengers = 10;
 
-// const fetchSwapi = (url) => {
-//   return fetch(url)
-//       .then((response) => response.json())
-//       .then((data) => data);
-// };
 
-// fetch function
-const fetchSwapi = (url, callback) => {
-  fetch(url).then((response) => response.json())
-      .then((data) => callback(data));
+const checkPassengers = (passengers, num) => {
+  return parseFloat(passengers) > num;
 };
 
-const checkPassengers = (data) => {
-  if (parseFloat(data['passengers']) > 10) {
-    checkAutonomy(data);
-  } else {
-    console.log('nop');
-    return false;
-  }
-};
-
-const checkAutonomy = (data) => {
+const checkAutonomy = (consumables) => {
   let result = false;
-  const arr = data['consumables'].split(' ');
+  const arr = consumables.split(' ');
   if (arr.length > 1) {
     if (arr[1][0].match(/[wmy]/)) {
       result = true;
@@ -31,66 +16,75 @@ const checkAutonomy = (data) => {
       arr[0] >= 7 ? result = true : result = false;
     }
   }
-  if (result === true) {
-    checkMovies(data);
-  } else {
-    console.log('nop');
-    return false;
+  return result;
+};
+
+
+const checkMovies = async (movieUrls) => {
+  let i = 0;
+  while (i < movieUrls.length) {
+    const value = await fetchMovie(movieUrls[i]);
+    if (value) {
+      return true;
+      break;
+    }
+    i += 1;
   }
 };
 
-const checkMovieId = (data) => {
-  if ([4, 5, 6].includes(data['episode_id'])) {
-    console.log('huuuuuurra');
-    return true;
-  } else {
-    console.log('nooooooop');
-    return false;
-  }
+const fetchMovie = (movie) => {
+  return fetch(movie)
+      .then((response) => response.json())
+      .then((data) => {
+        return [4, 5, 6].includes(data['episode_id']);
+      });
 };
 
-const checkMovies = (data) => {
-  const movies = data['films'];
-  movies.forEach((movieUrl) => {
-    fetchSwapi(movieUrl, checkMovieId);
-  });
-};
 
-fetchSwapi(url, checkPassengers);
+fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const enoughSpace = checkPassengers(data['passengers'], numberOfPassengers);
+      const enoughAutonomy = checkAutonomy(data['consumables']);
+      if (enoughSpace && enoughAutonomy) {
+        checkMovies(data['films']).then((goodMovies) => {
+          if (goodMovies) {
+            console.log(data);
+          } else {
+            console.log('dont belong to good movies');
+          }
+        });
+      } else {
+        console.log('not enough space or not enough autonomy');
+      }
+    });
 
-// fetch(url)
-//     .then((response) => response.json())
-//     .then((data) => {
-//       const starShips = data['results'];
 
-//       starShips.forEach((starShip) => {
-//         // if (parseFloat(starShip['passengers']) > 10) {
-//         //   console.log('hello')
-//         // }
 
-//         // if (checkAutonomy(starShip['consumables'])) {
-//         //   console.log('yes');
-//         // }
 
-//         console.log(checkMovies(starShip['films']));
 
-//       })
+// !async function(){
+// let data = await fetch("https://raw.githubusercontent.com/IbrahimTanyalcin/LEXICON/master/lexiconLogo.png")
+//     .then((response) => response.blob())
+//     .then(data => {
+//         return data;
 //     })
+//     .catch(error => {
+//         console.error(error);
+//     });
+
+// console.log(data);
+// }();
 
 
-// fetchSwapi(url)
-//     .then((data) => console.log(result));
+// // Example POST method implementation:
+// async function postData(url) {
+//   // Default options are marked with *
+//   const response = await fetch(url)
+//   return response.json(); // parses JSON response into native JavaScript objects
+// }
 
-
-
-
-// another functions
-// const dataFunction = (data) => {
-//   document.getElementById("myDiv").innerHTML = data['results'][0]['name'];
-//   console.log(data['results'][0]);
-// };
-
-
-
-// fetchSwapi(url, dataFunction);
-
+// postData(url)
+//   .then(data => {
+//     console.log(data); // JSON data parsed by `response.json()` call
+//   });
