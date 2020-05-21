4c2a2aec4d71ed4ac8cfbf321ce27029e927fa8a
renaud dor <renaud.dor@gmail.com>
2020-05-21 13:31:55 +0200

    add gitignore for linter files and start fetching swapi

--- swapi/js.js
+++ swapi/js.js
@@ -1,7 +1,15 @@
+url = 'https://swapi.dev/api/starships/';
 
-console.log('hey');
+fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const starShips = data['results'];
 
+      starShips.forEach((starShip) => {
+        console.log(starShip['passengers']);
+        if (starShip['passengers'] > 10) {
+          console.log('hello')
+        }
 
-var myVar = 'hello'
-
-console.log(myVar)
+      })
+    })
