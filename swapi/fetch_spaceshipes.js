9770e3b36416d38ac47009647fb2654c75e011f2
renaud dor <renaud.dor@gmail.com>
2020-05-22 13:47:07 +0200

    finish fastest ship exercise

--- swapi/fetch_spaceshipes.js
+++ swapi/fetch_spaceshipes.js
@@ -1,5 +1,6 @@
-const spaceShipsUrl = 'https://swapi.dev/api/starships/';
-
+const fetchData = (url) => {
+  return fetch(url).then((response) => response.json());
+};
 
 const checkPassengers = (passengers, passengerQty) => {
   return parseFloat(passengers) > passengerQty;
@@ -7,50 +8,39 @@ const checkPassengers = (passengers, passengerQty) => {
 
 const checkAutonomy = (consumables) => {
   let result = false;
-  const arr = consumables.split(' ');
-  if (arr.length > 1) {
-    if (arr[1][0].match(/[wmy]/)) {
+  const autonomy = consumables.split(' ');
+  if (autonomy.length > 1) {
+    if (autonomy[1][0].match(/[wmy]/)) {
       result = true;
-    } else if (arr[1][0] === 'd') {
-      arr[0] >= 7 ? result = true : result = false;
+    } else if (autonomy[1][0] === 'd') {
+      autonomy[0] >= 7 ? result = true : result = false;
     }
   }
   return result;
 };
 
-
 const checkMovies = async (movieUrls) => {
   let i = 0;
   while (i < movieUrls.length) {
-    const value = await fetchMovie(movieUrls[i]);
-    if (value) {
+    const goodMovie = await fetchData(movieUrls[i])
+        .then((data) => [4, 5, 6].find((episodeId) => episodeId === data['episode_id']));
+    if (goodMovie) {
       return true;
-      break;
     }
     i += 1;
   }
 };
 
-const fetchMovie = (movie) => {
-  return fetch(movie)
-      .then((response) => response.json())
+const fetchShips = async (url, passengerQty) => {
+  const ships = await fetchData(url)
       .then((data) => {
-        return [1, 2, 3].includes(data['episode_id']);
-      });
-};
-
-
-const fetchSpaceships = async (url, passengerQty) => {
-  const results = await fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        return data['results'].map((spaceShip) => {
-          const enoughSpace = checkPassengers(spaceShip['passengers'], passengerQty);
-          const enoughAutonomy = checkAutonomy(spaceShip['consumables']);
+        return data['results'].map((ship) => {
+          const enoughSpace = checkPassengers(ship['passengers'], passengerQty);
+          const enoughAutonomy = checkAutonomy(ship['consumables']);
           if (enoughSpace && enoughAutonomy) {
-            return checkMovies(spaceShip['films']).then((goodMovies) => {
+            return checkMovies(ship['films']).then((goodMovies) => {
               if (goodMovies) {
-                return {'name': spaceShip['name'], 'speed': spaceShip['MGLT']};
+                return {'name': ship['name'], 'speed': ship['MGLT']};
               } else {
                 // return 'dont belong to good movies';
                 return false;
@@ -63,15 +53,15 @@ const fetchSpaceships = async (url, passengerQty) => {
         });
       });
 
-  return Promise.all(results);
+  return Promise.all(ships);
 };
 
-const fetchAll = async (url, passengerQty) => {
+const fetchAllShips = async (url, passengerQty) => {
   const results = [];
 
   while (url) {
-    results.push(await fetchSpaceships(url, passengerQty));
-    url = await fetch(url).then((response) => response.json()).then((data) => data['next']);
+    results.push(await fetchShips(url, passengerQty));
+    url = await fetchData(url).then((data) => data['next']);
   }
 
 
@@ -80,44 +70,56 @@ const fetchAll = async (url, passengerQty) => {
   let maxSpeed = 0;
   let fastestShip = '';
   let fastestShipId = 0;
-  cleanedResults.forEach((ship, i) => {
-    if (parseInt(ship['speed']) > maxSpeed) {
-      maxSpeed = parseInt(ship['speed']);
-      fastestShip = `${ship['name']} - (speed: ${ship['speed']} MGLT)`;
-      fastestShipId = i;
+
+  if (cleanedResults.length > 1) {
+    cleanedResults.forEach((ship, i) => {
+      if (parseInt(ship['speed']) > maxSpeed) {
+        maxSpeed = parseInt(ship['speed']);
+        fastestShip = `${ship['name']} (speed: ${ship['speed']} MGLT)`;
+        fastestShipId = i;
+      }
+    });
+
+    if (!fastestShip) {
+      fastestShip = `${cleanedResults[0]['name']} (unknown speed...)`;
     }
-  });
-
-  cleanedResults.splice(fastestShipId);
-
-
-  if (!fastestShip) {
-    fastestShip = `${cleanedResults[0]['name']} (unknown speed...)`;
   }
 
-  displayResults(cleanedResults, fastestShip);
+  displayResults(cleanedResults, fastestShip, fastestShipId);
 };
 
 
-const displayResults = (ships, fastestShip) => {
+const displayResults = (ships, fastestShip, fastestShipId) => {
   fastestShipDiv.innerHTML = '';
   otherResultsDiv.innerHTML = '';
-  fastestShipDiv.innerHTML = `
-    <p>The fastest starwars ship for your search is:</p>
-    <p><span>${fastestShip}</span></p>`;
 
-  otherResultsDiv.innerHTML = '<b>Here are other good candidates:</b>';
-  ships.forEach((ship) => {
-    otherResultsDiv.insertAdjacentHTML('beforeend', `<p>${ship['name']}</p>`);
-  });
+  if (fastestShip) {
+    fastestShipDiv.innerHTML = `
+      <p>The fastest starwars ship for your search is:</p>
+      <p><span>${fastestShip}</span></p>`;
+  } else {
+    fastestShipDiv.innerHTML = 'No fastest ship...';
+  }
+
+  if (ships.length > 2) {
+    otherResultsDiv.innerHTML = '<b>Here are other good candidates:</b>';
+    ships.forEach((ship, i) => {
+      if (i != fastestShipId) {
+        otherResultsDiv.insertAdjacentHTML('beforeend', `<p>${ship['name']}</p>`);
+      }
+    });
+  } else {
+    otherResultsDiv.innerHTML = '<b>No other results...</b>';
+  }
 };
 
-
+const spaceShipsUrl = 'https://swapi.dev/api/starships/';
 const input = document.querySelector('.qty-input');
 const btn = document.querySelector('button');
 const fastestShipDiv = document.querySelector('.fastest-ship');
 const otherResultsDiv = document.querySelector('.other-results');
 
+
 btn.addEventListener('click', (event) => {
-  fetchAll(spaceShipsUrl, input.value);
+  fetchAllShips(spaceShipsUrl, input.value);
 });
